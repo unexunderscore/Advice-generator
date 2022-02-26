7dc91fca8107f1c315dab2ba682c8b2b76372b6f
Attila Tóth <toth_attila@outlook.hu>
2022-02-26 21:44:59 +0100

    Add Fetch to get random qoute, add cube icon that generate new advice onclick. Also made resopnsive to mobil display.

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 // Checking the connection between js and html 
 const greetings = "Hello there!";
 console.log(greetings);
+
+
+function randomFunction() {
+location.reload(); 
+}   
+fetch('https://api.adviceslip.com/advice')
+    .then( (data) => data.json())
+    .then( (advice) => generateAdvice(advice))
+
+
+const generateAdvice = (data) => {
+   const adviceText = data.slip.advice
+   let adviceId = data.slip.id
+   const divId = document.querySelector('.advice-id')
+   const divAdvice = document.querySelector('.random-advice')
+
+    divId.innerHTML = adviceId
+    divAdvice.innerHTML = adviceText
+}
+
+
