6ef6df6d6dca7e0d04179e915bceca860167e83e
Attila Tóth <toth_attila@outlook.hu>
2022-02-27 07:28:14 +0100

    Add cache no-store option to fetch, now the app working proprely with firefox browser too. Changed onclick to event listener and not using anymore page realod to get new advice.

--- script.js
+++ script.js
@@ -3,23 +3,26 @@ const greetings = "Hello there!";
 console.log(greetings);
 
 
+const dice = document.getElementById("dice");
+
+
 function randomFunction() {
-location.reload(); 
-}   
 
-fetch('https://api.adviceslip.com/advice')
-    .then( (data) => data.json())
-    .then( (advice) => generateAdvice(advice))
-
+fetch('https://api.adviceslip.com/advice', {cache: "no-store"})
+    .then((data) => data.json())
+    .then((advice) => generateAdvice(advice))
+}
+randomFunction();
 
 const generateAdvice = (data) => {
-   const adviceText = data.slip.advice
+   let adviceText = data.slip.advice
    let adviceId = data.slip.id
    const divId = document.querySelector('.advice-id')
    const divAdvice = document.querySelector('.random-advice')
 
     divId.innerHTML = adviceId
     divAdvice.innerHTML = adviceText
-}
+    console.log(adviceText)
+} 
 
-
+dice.addEventListener("click", randomFunction);
